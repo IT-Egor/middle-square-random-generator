fe227810d46ec78330e34ded35866d127e54c100
IT-Egor <egor.belousov.04@yandex.ru>
2024-09-29 23:58:21 +0300

    Cleaning the code

--- src/Main.java
+++ src/Main.java
@@ -19,28 +19,5 @@ public class Main {
             action = scanner.next();
             gr.remote(action);
         } while (!action.equals("0"));
-
-//        RandomGenerator generator = new RandomGenerator(1726506092498L, Mode.MANUAL);
-//        generator = new RandomGenerator();
-//        LR2Distribution distribution = new LR2Distribution();
-//        List<Double> normal = generator.generateNormalDistributionSequence();
-//        List<Double> dist = generator.generateSequenceWithDistribution(normal, distribution);
-//        Histogram histogram = new Histogram(50, distribution);
-//        histogram.addListToHist(dist);
-//        String pythonData = histogram.getHist().toString().replace("[", "").replace("]", "");
-////        PythonExecutor.execute("src/randomgenerator/utility/evaluateNormal.py", pythonData);
-//
-//        List<Integer> pSegments = histogram.getProbabilityOfHittingTheSegment();
-//
-//        System.out.println("histogram.getHist() = " + histogram.getHist());
-//        System.out.println("histogram.getHistSum() = " + histogram.getHistSum());
-//        System.out.println("pSegments = " + pSegments);
-//        System.out.println("pSegments.size() = " + pSegments.size());
-//
-//        int sum = 0;
-//        for (int pSegment : pSegments) {
-//            sum += pSegment;
-//        }
-//        System.out.println("sum = " + sum);
     }
 }
